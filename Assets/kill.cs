b0dd4f5bb29a48cea5bc7dad5b8024e2f15db2cd
Matt Hines <cobalthex@live.com>
2016-01-31 13:57:57 -0800

    adding killing field

---
+++ Assets/kill.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kill : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        var plyr = other.GetComponent<PlayerController>();
+        if (plyr)
+        {
+            print(plyr);
+        }
+    }
+}
