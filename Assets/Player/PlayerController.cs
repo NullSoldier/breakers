97cb02a5cf1da9598c31dfc79add9c5e3694e052
Jason Spafford <nullprogrammer@gmail.com>
2016-01-31 13:56:47 -0800

    Added player slamming

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,16 +4,53 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float Speed = -0.5f;
+	private PlayerState state;
+	private PlayerState lastState;
+	private float lastStateTime;
+
+	enum PlayerState {
+		Alive,
+		SlammingUp,
+		SlammingDown,
+		Dead,
+		Inactive
+	}
 
 	void Start ()
 	{
+		state = lastState = PlayerState.Alive;
 	}
 
 	void Update ()
 	{
-		var h = Input.GetAxis("Horizontal");
-		var v = Input.GetAxis("Vertical");
-		transform.position += new Vector3(h, 0, v) * Speed;
-	
+		if (state != lastState) {
+			if (state == PlayerState.SlammingUp) {
+				var body = GetComponent<Rigidbody> ();
+				body.velocity = Vector3.up * 5;
+			} else if (state == PlayerState.SlammingDown) {
+				var body = GetComponent<Rigidbody> ();
+				body.velocity = Vector3.down * 10;
+			}
+
+			lastState = state;
+			lastStateTime = Time.time;
+		} else {
+			float timeSinceLast = Time.time - lastStateTime;
+
+			if (state == PlayerState.Alive) {
+				var h = Input.GetAxis ("Horizontal");
+				var v = Input.GetAxis ("Vertical");
+				transform.position += new Vector3 (h, 0, v) * Speed;
+
+				if (Input.GetKey (KeyCode.Space)) {
+					state = PlayerState.SlammingUp;
+				}
+			} else if (state == PlayerState.SlammingUp && timeSinceLast > 0.25) {
+				state = PlayerState.SlammingDown;
+			} else if (state == PlayerState.SlammingDown && timeSinceLast > 0.25) {
+				state = PlayerState.Alive;
+			}
+
+		}
 	}
 }
